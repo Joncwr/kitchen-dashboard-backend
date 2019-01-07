b4c5221e12bd93ad167cc90c8d4cb76de381fb45
Joncwr <joncwrrr@gmail.com>
2019-01-07 12:11:52 +0800

    no message

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const bodyParser = require('body-parser')
+const io = require('socket.io')();
+const axios = require("axios");
+
+app.use(cors())
+app.use(bodyParser.json())
+const serverPort = process.env.PORT || 24601;
+
+app.listen(serverPort, () => console.log(`Listening on port ${serverPort}!!!!`));
+
+// io.on('connection', (client) => {
+//   client.on('subscribeToTimer', (interval) => {
+//     console.log('client is subscribing to timer with interval ', interval);
+//     setInterval(() => {
+//       client.emit('timer', new Date());
+//     }, interval);
+//   });
+// });
+
+io.on("connection", socket => {
+  setInterval(() => {
+    socket.emit("timer", 'lol');
+  }, 1000)
+});
+
+const port = 8000;
+io.listen(port);
+console.log('listening on port ', port);
